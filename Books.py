d89ae976e893e2a4a88dfa1467bff763f22fd56c
Bora Saglam <posarobit@gmail.com>
2025-06-11 12:51:41 +0300

    Books.py ekledim.

---
+++ Books.py
@@ -0,0 +1,22 @@
+print("Books")
+print("")
+
+print("Rebecca Yarros's Books")
+print("")
+
+print("Empyrean Books Series")
+print("")
+
+print("1. Fourth Wing")
+print("2. Iron Flame")
+print("3. Onyx Storm")
+
+print("Flight & Glory Books Seris")
+print("")
+
+print("1. Full Measures")
+print("2. Eyes Turned Skyward")
+print("3. Beyond What is Given")
+print("4. Hallowed Ground")
+print("5. The Reality of Everything")
+print("")
