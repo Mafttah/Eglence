48800652f288972e3af374fb72e1f91642beeeb9
Bora Saglam <posarobit@gmail.com>
2025-06-11 12:45:06 +0300

    Travellin.py ekledim.

---
+++ Travellin.py
@@ -0,0 +1,42 @@
+print("Şehirler ve Gidilecek Yerler")
+
+print(" 1. Thesalanoki ( Selanik )")
+
+print("Aristotales Meydanı")
+print("Atatürk Evi Müzesi")
+print("Ano Poli")
+print("Beyaz Kule")
+print("Tsimiski Street")
+
+
+print(" 2. Athena")
+
+print("Plaka")
+print("Monastraki")
+print("Temple of Ares")
+print("Temple of Hephaestus")
+print("Philopappos Hill")
+print("Athena Akropolis")
+print("Temple of Olympian Zeus")
+print("Ulusal Bahçe")
+-print("Lycabettus Tepesi")
+print("Syntagma Meydanı")
+
+
+print("Patras' tan feribota bineceğiz.")
+
+
+print(" 3. Bari")
+print("Bari Katedrali")
+-print("Piazza Del Ferrarese")
+-print("Lido San Francesco")
+
+
+print(" 4. Napoli")
+print("-Royale Palace of Naples")
+print("Castel Nuovo")
+print("Castel dell’Ovo")
+print("Pompei Antik Kenti")
+print("Via Toledo")
+print("Castel Sant’Elmo")
+print("Santa Chiara Manastırı")
